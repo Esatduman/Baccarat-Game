888482d0e7403faf776685534ccc976c062a788b
jjuarez19 <89400644+jjuarez19@users.noreply.github.com>
2023-10-20 13:58:36 -0500

    added method signatures and created extra classes

---
+++ JavaFX_Project2_Fall23/src/main/java/BaccaratDealer.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class BaccaratDealer extends Card{
+	private ArrayList<Card> deck;
+	
+	public void generateDeck() {
+		
+	}
+	
+	public ArrayList<Card> dealHand(){
+		
+	}
+	
+	public Card drawOne() {
+		
+	}
+	
+	public void shuffleDeck() {
+		
+	}
+	
+	public int deckSize() {
+		
+	}
+}
+
---
+++ JavaFX_Project2_Fall23/src/main/java/Card.java
@@ -0,0 +1,9 @@
+
+public class Card {
+	private String suite;
+	private int value;
+	
+	Card(String theSuite, int theValue){
+		
+	}
+}
